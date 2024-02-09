1cdc24544ffc9616a237ba337e5b6d5bf157df19
Wag 31 <rupcyes@mail.ru>
2024-02-09 17:53:59 +0300

    два новых контроллера

---
+++ HouYun3/Controllers/SubscriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouYun3.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
