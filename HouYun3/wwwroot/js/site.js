3a8a180726ad9855cbd177a3c1d715a8eea20ff6
Wag 31 <rupcyes@mail.ru>
2024-02-10 22:35:21 +0300

    добавил новый плеер, пока без модулей

--- HouYun3/wwwroot/js/site.js
+++ HouYun3/wwwroot/js/site.js
@@ -1,4 +1,5 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const player = new Plyr('#videoPlayer', {
+        controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen']
+    });
+});
