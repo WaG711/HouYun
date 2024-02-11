e9294655d1ebedbc643d565cec11275846c85855
Wag 31 <rupcyes@mail.ru>
2024-02-11 19:23:05 +0300

    delete plyr

--- HouYun3/wwwroot/js/site.js
+++ HouYun3/wwwroot/js/site.js
@@ -1,5 +1,1 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    const player = new Plyr('#videoPlayer', {
-        controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen']
-    });
-});
+﻿
