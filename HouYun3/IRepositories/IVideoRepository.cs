de80aa7d30484648a0a627462615ace2fa6c5ebc
Wag 31 <rupcyes@mail.ru>
2024-02-14 19:28:52 +0300

    forPull

--- HouYun3/IRepositories/IVideoRepository.cs
+++ HouYun3/IRepositories/IVideoRepository.cs
@@ -6,11 +6,6 @@ namespace HouYun3.IRepositories
     {
         Task<IEnumerable<Video>> GetAllVideos();
         Task<IEnumerable<Video>> GetVideosByCategory(string categoryName);
-        Task<IEnumerable<Video>> GetAllVideosExceptId(Guid id);
         Task<Video> GetVideoById(Guid id);
-        Task AddVideo(Video video, IFormFile videoFile, IFormFile posterFile);
-        Task<Video> UpdateVideo(Video video);
-        Task DeleteVideo(Guid id);
-        Task<IEnumerable<Video>> GetVideosByChannelId(Guid channelId);
     }
 }
