a9e13ebf25253e2608f8c5618efd90f1f7e4fb5d
Wag 31 <rupcyes@mail.ru>
2024-02-18 17:09:49 +0300

    Index Video, WH, WL, Channel

--- HouYun/wwwroot/js/site.js
+++ HouYun/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿function toggleMenu(videoId) {
+    var menu = document.getElementById("menuItems_" + videoId);
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
