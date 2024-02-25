4aaaa66e3c5ac692e6dbc8cef228a12fe0dae6a3
NIK NIK <nikitanik10305@outlook.com>
2024-02-25 04:17:39 +0300

    Изменения в регистраций и логин маленький фикс history

--- HouYun/ViewModels/forUser/LoginViewModel.cs
+++ HouYun/ViewModels/forUser/LoginViewModel.cs
@@ -5,15 +5,12 @@ namespace HouYun.ViewModels.forUser
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Логин")]
-        [Display(Name = "Логин")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
-        [Display(Name = "Запомнить?")]
         public bool RememberMe { get; set; }
     }
 }
--- HouYun/ViewModels/forUser/RegisterViewModel.cs
+++ HouYun/ViewModels/forUser/RegisterViewModel.cs
@@ -4,18 +4,13 @@ namespace HouYun.ViewModels.forUser
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Почта занята")]
         [EmailAddress]
-        [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [Display(Name = "Username")]
-        [MaxLength(20, ErrorMessage = "Имя должно иметь длину меньше 20 символов")]
-        [MinLength(3, ErrorMessage = "Имя должно иметь длину больше 3 символов")]
+        [MaxLength(20)]
+        [MinLength(3)]
         public string UserName { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
