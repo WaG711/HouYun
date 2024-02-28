ab1017c4c58a10ebb1b61e25ddff3a1e2949625c
Wag 31 <rupcyes@mail.ru>
2024-02-28 16:42:21 +0300

    лишние маршруты?

--- HouYun/Program.cs
+++ HouYun/Program.cs
@@ -4,11 +4,7 @@ using HouYun.IRepositories;
 using HouYun.Repositories;
 using Microsoft.AspNetCore.Identity;
 using HouYun.Models;
-using Microsoft.Data.SqlClient;
-using HouYun.Controllers;
-using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Http.Features;
 
 
@@ -98,52 +94,14 @@ namespace HouYun
                 pattern: "{controller=Video}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
-                name: "Notifications",
-                pattern: "Notifications",
-                defaults: new { controller = "Notifications", action = "Index" });
-
-            app.MapControllerRoute(
-                name: "AddView",
-                pattern: "View/AddView/{videoId}",
-                defaults: new { controller = "View", action = "AddView" });
-
-            app.MapControllerRoute(
-                name: "AddToWatchHistory",
-                pattern: "WatchHistory/AddToWatchHistory/{videoId}",
-                defaults: new { controller = "WatchHistory", action = "AddToWatchHistory" });
-
-            app.MapControllerRoute(
                 name: "addComment",
                 pattern: "Comment/AddComment",
                 defaults: new { controller = "Comment", action = "AddComment" });
 
             app.MapControllerRoute(
-                name: "search",
-                pattern: "Video/Search",
-                defaults: new { controller = "Search", action = "Search" });
-
-            app.MapControllerRoute(
                  name: "channel",
                  pattern: "Channel/{channelName}",
                  defaults: new { controller = "Channel", action = "Index" });
-
-            app.MapControllerRoute(
-                name: "search",
-                pattern: "Search/SearchResult/{searchTerm?}",
-                defaults: new { controller = "Search", action = "SearchResult" }
-            );
-
-            app.MapControllerRoute(
-                name: "login",
-                pattern: "login",
-                defaults: new { controller = "Login" }
-            );
-
-            app.MapControllerRoute(
-                name: "registration",
-                pattern: "registration",
-                defaults: new { controller = "Registration"}
-            );
 
             app.Run();
         }
