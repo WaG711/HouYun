5a69da2ccb6a1486e9afe8aa2ebc981c836afdb6
NIK NIK <nikitanik10305@outlook.com>
2024-02-29 05:21:56 +0300

    Сделал scrollbar пиздатым

--- HouYun/ViewModels/forUser/RegisterViewModel.cs
+++ HouYun/ViewModels/forUser/RegisterViewModel.cs
@@ -8,7 +8,7 @@ namespace HouYun.ViewModels.forUser
         public string Email { get; set; }
 
         [MaxLength(20)]
-        [MinLength(3)]
+        [MinLength(4)]
         public string UserName { get; set; }
 
         [DataType(DataType.Password)]
