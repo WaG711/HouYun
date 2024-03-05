44d414e42440f50dc0d2bdce262ef8cfd2e8a822
NIK NIK <nikitanik10305@outlook.com>
2024-03-05 06:41:42 +0300

    Логин готов

--- HouYun/ViewModels/forUser/ChangePasswordViewModel.cs
+++ HouYun/ViewModels/forUser/ChangePasswordViewModel.cs
@@ -4,10 +4,10 @@ namespace HouYun.ViewModels.forUser
 {
     public class ChangePasswordViewModel
     {
-        [Required(ErrorMessage = "Поле Новый пароль обязательно для заполнения.")]
+        [Required(ErrorMessage = "Поле Новый пароль обязательно для заполнения")]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
-        [Required(ErrorMessage = "Поле Старый пароль обязательно для заполнения.")]
+        [Required(ErrorMessage = "Поле Старый пароль обязательно для заполнения")]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
     }
--- HouYun/ViewModels/forUser/LoginViewModel.cs
+++ HouYun/ViewModels/forUser/LoginViewModel.cs
@@ -4,10 +4,8 @@ namespace HouYun.ViewModels.forUser
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Логин")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
