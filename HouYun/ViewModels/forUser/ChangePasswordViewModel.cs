2e0394a11bd581b088c83752ed7d4a4333191f76
NIK NIK <nikitanik10305@outlook.com>
2024-03-05 05:56:08 +0300

    Готов вывод ошибок при смене имени или пароля

--- HouYun/ViewModels/forUser/ChangePasswordViewModel.cs
+++ HouYun/ViewModels/forUser/ChangePasswordViewModel.cs
@@ -4,12 +4,10 @@ namespace HouYun.ViewModels.forUser
 {
     public class ChangePasswordViewModel
     {
-        [Required]
-        [Display(Name = "Новый Пароль")]
+        [Required(ErrorMessage = "Поле Новый пароль обязательно для заполнения.")]
         [DataType(DataType.Password)]
         public string NewPassword { get; set; }
-        [Required]
-        [Display(Name = "Старый Пароль")]
+        [Required(ErrorMessage = "Поле Старый пароль обязательно для заполнения.")]
         [DataType(DataType.Password)]
         public string OldPassword { get; set; }
     }
--- HouYun/ViewModels/forUser/ChangeUserNameViewModel.cs
+++ HouYun/ViewModels/forUser/ChangeUserNameViewModel.cs
@@ -4,10 +4,10 @@ namespace HouYun.ViewModels.forUser
 {
     public class ChangeUserNameViewModel
     {
-        [Required]
-        [StringLength(15, MinimumLength = 4)]
+        [Required(ErrorMessage = "Поле Никнейм обязательно для заполнения.")]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Длина поля Никнейм должна быть от 4 до 15 символов.")]
         public string NewUserName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Поле Пароль обязательно для заполнения.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
