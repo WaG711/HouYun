592e8212c9b518a4522b26b180c23700f1d45d07
NIK NIK <nikitanik10305@outlook.com>
2024-03-05 23:19:43 +0300

    Изменил ошибки

--- HouYun/ViewModels/forUser/RegistrationViewModel.cs
+++ HouYun/ViewModels/forUser/RegistrationViewModel.cs
@@ -4,14 +4,15 @@ namespace HouYun.ViewModels.forUser
 {
     public class RegistrationViewModel
     {
+        [Required(ErrorMessage = "Почта обязательна для заполнения")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Логин обязательно для заполнения.")]
+        [Required(ErrorMessage = "Логин обязателен для заполнения")]
         [StringLength(15, MinimumLength = 4, ErrorMessage = "Длина Никнейм должна быть от 4 до 15 символов")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "Пароль обязательно для заполнения.")]
+        [Required(ErrorMessage = "Пароль обязателен для заполнения")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
